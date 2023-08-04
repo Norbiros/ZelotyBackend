6979a0496b17d2bce76ed215892ce45d26d14745
Norbiros <norbiros@protonmail.com>
2023-08-04 21:12:41 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.get("/*", function (req, res) {
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/`);
+    console.log(`Server is running on port ${PORT}!`);
 });
 
 export { app };
